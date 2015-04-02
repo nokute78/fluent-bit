71368051deb01dbf029146baa3056adad71cb716
Eduardo Silva <eduardo@treasure-data.com>
2015-04-02 10:25:18 -0600

    wip
    Signed-off-by: Eduardo Silva <eduardo@treasure-data.com>

--- src/flb_pack.c
+++ src/flb_pack.c
@@ -18,12 +18,14 @@
  */
 
 #include <stdlib.h>
+#include <string.h>
 
+#include <msgpack.h>
 #include <jsmn/jsmn.h>
 #include <fluent-bit/flb_error.h>
 #include <fluent-bit/flb_utils.h>
 
-static jsmntok_t *json_tokenise(char *js, size_t len)
+static jsmntok_t *json_tokenise(char *js, size_t len, int *arr_size)
 {
     int ret;
     unsigned int n;
@@ -41,14 +43,19 @@ static jsmntok_t *json_tokenise(char *js, size_t len)
     }
 
     if (ret == JSMN_ERROR_INVAL) {
+        printf("1\n");
         flb_utils_error(FLB_ERR_JSON_INVAL);
         goto error;
     }
 
     if (ret == JSMN_ERROR_PART) {
+        printf("2\n");
         flb_utils_error(FLB_ERR_JSON_PART);
         goto error;
     }
+
+    /* Store the array length */
+    *arr_size = n;
     return tokens;
 
  error:
@@ -57,15 +64,88 @@ static jsmntok_t *json_tokenise(char *js, size_t len)
 }
 
 /* It parse a JSON string and convert it to MessagePack format */
-char *flb_pack_json(char *js, size_t len, int *size)
+char *flb_pack_json(char *tag, char *js, size_t len, int *size)
 {
+    int i;
+    int flen;
+    int arr_size;
+    int tag_len;
+    int expect;
+    char *p;
+    char *buf;
+    jsmntok_t *t;
     jsmntok_t *tokens;
+    msgpack_packer pck;
+    msgpack_sbuffer sbuf;
 
-    tokens = json_tokenise(js, len);
-    if (!tokens) {
+    if (!tag || !js) {
+        printf("1\n");
         return NULL;
     }
 
+    tokens = json_tokenise(js, len, &arr_size);
+    if (!tokens) {
+        printf("2\n");
+        return NULL;
+    }
 
-    return NULL;
+    tag_len = strlen(tag);
+
+    printf("JSON=\n%s\n", js);
+
+    /* initialize buffers */
+    msgpack_sbuffer_init(&sbuf);
+    msgpack_packer_init(&pck, &sbuf, msgpack_sbuffer_write);
+
+    for (i = 0; i < arr_size ; i++) {
+        t = &tokens[i];
+
+        printf("i=%i start=%i end=%i\n", i, t->start, t->end);
+        if (t->start == -1 || t->end == -1 || (t->start == 0 && t->end == 0)) {
+            break;
+        }
+        flen = (t->end - t->start);
+
+        switch (t->type) {
+        case JSMN_OBJECT:
+            /* Each object must start with a string */
+            expect = JSMN_STRING;
+            msgpack_pack_map(&pck, t->size);
+            break;
+        case JSMN_ARRAY:
+            msgpack_pack_array(&pck, t->size);
+            printf("i=%i is ARRAY\n", i);
+            break;
+        case JSMN_STRING:
+            msgpack_pack_raw(&pck, flen);
+            msgpack_pack_raw_body(&pck, js + t->start, flen);
+            if (expect == JSMN_STRING) {
+                /* String is only mandatory */
+            }
+            printf("i=%i is STRING\n", i);
+            break;
+        case JSMN_PRIMITIVE:
+            p = js + t->start;
+            if (strncmp(p, "false", 5) == 0) {
+                msgpack_pack_false(&pck);
+            }
+            else if (strncmp(p, "true", 4) == 0) {
+                msgpack_pack_true(&pck);
+            }
+            else if (strncmp(p, "null", 4) == 0) {
+                msgpack_pack_nil(&pck);
+            }
+            printf("i=%i is PRIMITIVE\n", i);
+            break;
+        }
+    }
+
+    /* dump data back to a new buffer */
+    *size = sbuf.size;
+    buf = malloc(sbuf.size);
+    memcpy(buf, sbuf.data, sbuf.size);
+    msgpack_sbuffer_destroy(&sbuf);
+
+    free(tokens);
+    return buf;
 }
